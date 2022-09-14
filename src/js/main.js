09a7382f997ad58ff8efd4475543b485356c182b
max_air <max_air@bk.ru>
2022-09-14 16:33:17 +0300

    Работа над формой отправки данных

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,26 @@ import tabs from "./modules/tabs";
 import pictures from "./modules/pictures";
 import styles from "./modules/styles";
 import accordion from "./modules/accordion";
+import modal from "./modules/modal";
+import form from "./modules/form";
+
+let timeout = setTimeout(() => {
+    document.querySelectorAll("[data-modal]").forEach(modal => {
+        if (getComputedStyle(modal).display == "block") {
+            clearTimeout(timeout);
+        } else {
+            document.querySelector(".popup-consultation").style.display = "block";
+            document.body.style.overflow = "hidden";
+        }
+    });
+
+}, 60000);
+export { timeout };
 
 window.addEventListener("DOMContentLoaded", () => {
+
+
+
     new WOW().init();
     sliderVertical(3000);
     feedbackSlider(2000);
@@ -15,4 +33,9 @@ window.addEventListener("DOMContentLoaded", () => {
     pictures();
     styles();
     accordion();
+    modal(".button-design", ".popup-design");
+    modal(".button-consultation", ".popup-consultation");
+    form();
+
 });
+
---
+++ src/js/modules/modal.js
@@ -0,0 +1,38 @@
+import { timeout } from "../main";
+
+export default function modal(triggerSelector, modalSelector) {
+    const trigger = document.querySelectorAll(triggerSelector);
+    const modalWindow = document.querySelector(modalSelector);
+    const allModals = document.querySelectorAll("[data-modal]");
+
+    function openModal() {
+        modalWindow.style.display = "block";
+        document.body.style.overflow = "hidden";
+        clearTimeout(timeout);
+    }
+
+    function closeModal() {
+        allModals.forEach(modal => {
+            modal.style.display = "none";
+        });
+        // modalWindow.style.display = "none";
+        document.body.style.overflow = "";
+    }
+
+    trigger.forEach(btn => {
+        btn.addEventListener("click", openModal);
+    });
+
+    modalWindow.addEventListener("click", e => {
+        if (e.target && (e.target.classList.contains(modalSelector.replace(/\./, "")) || e.target.classList.contains("popup-close"))) {
+            closeModal();
+        }
+    });
+
+
+
+
+
+
+
+}
